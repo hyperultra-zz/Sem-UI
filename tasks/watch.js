5f447659559bd8c31055d970e4d3af7e154cdf3a
PGobz <p.gobin@gmail.com>
2018-11-11 15:35:14 -0800

    Update tasks/watch.js

--- tasks/watch.js
+++ tasks/watch.js
@@ -80,156 +80,156 @@ module.exports = function(callback) {
       Watch CSS
   ---------------*/
 
-  gulp
-    .watch([
-      source.config,
-      source.definitions   + '/**/*.less',
-      source.site          + '/**/*.{overrides,variables}',
-      source.themes        + '/**/*.{overrides,variables}'
-    ], function(file) {
+  const cssWatcher = gulp.watch([
+    source.config,
+    source.definitions   + '/**/*.less',
+    source.site          + '/**/*.{overrides,variables}',
+    source.themes        + '/**/*.{overrides,variables}'
+  ]);
 
-      var
-        lessPath,
+  cssWatcher.on('change', (path) => {
+    console.log(path);
 
-        stream,
-        compressedStream,
-        uncompressedStream,
+    var
+      lessPath,
 
-        isDefinition,
-        isPackagedTheme,
-        isSiteTheme,
-        isConfig
+      stream,
+      compressedStream,
+      uncompressedStream,
+
+      isDefinition,
+      isPackagedTheme,
+      isSiteTheme,
+      isConfig
+    ;
+
+    // log modified file
+    gulp.src(path)
+      .pipe(print(log.modified))
+    ;
+
+    /*--------------
+       Find Source
+    ---------------*/
+
+    // recompile on *.override , *.variable change
+    isConfig        = (path.indexOf('theme.config') !== -1 || path.indexOf('site.variables') !== -1);
+    isPackagedTheme = (path.indexOf(source.themes) !== -1);
+    isSiteTheme     = (path.indexOf(source.site) !== -1);
+    isDefinition    = (path.indexOf(source.definitions) !== -1);
+
+    if(isConfig) {
+      console.info('Rebuilding all UI');
+      // impossible to tell which file was updated in theme.config, rebuild all
+      buildCSS();
+      return;
+    }
+    else if(isPackagedTheme) {
+      console.log('Change detected in packaged theme');
+      lessPath = replaceExt(path, '.less');
+      lessPath = lessPath.replace(tasks.regExp.theme, source.definitions);
+    }
+    else if(isSiteTheme) {
+      console.log('Change detected in site theme');
+      lessPath = replaceExt(path, '.less');
+      lessPath = lessPath.replace(source.site, source.definitions);
+    }
+    else {
+      console.log('Change detected in definition');
+      lessPath = path;
+    }
+
+    /*--------------
+       Create CSS
+    ---------------*/
+
+    if( fs.existsSync(lessPath) ) {
+
+      // unified css stream
+      stream = gulp.src(lessPath)
+        .pipe(plumber(settings.plumber.less))
+        .pipe(less(settings.less))
+        .pipe(print(log.created))
+        .pipe(replace(comments.variables.in, comments.variables.out))
+        .pipe(replace(comments.license.in, comments.license.out))
+        .pipe(replace(comments.large.in, comments.large.out))
+        .pipe(replace(comments.small.in, comments.small.out))
+        .pipe(replace(comments.tiny.in, comments.tiny.out))
+        .pipe(autoprefixer(settings.prefix))
+        .pipe(gulpif(config.hasPermission, chmod(config.permission)))
       ;
 
-      // log modified file
-      gulp.src(file.path)
-        .pipe(print(log.modified))
+      // use 2 concurrent streams from same pipe
+      uncompressedStream = stream.pipe(clone());
+      compressedStream   = stream.pipe(clone());
+
+      uncompressedStream
+        .pipe(plumber())
+        .pipe(replace(assets.source, assets.uncompressed))
+        .pipe(gulp.dest(output.uncompressed))
+        .pipe(print(log.created))
+        .on('end', function() {
+          gulp.task('package uncompressed css')();
+        })
       ;
 
-      /*--------------
-         Find Source
-      ---------------*/
-
-      // recompile on *.override , *.variable change
-      isConfig        = (file.path.indexOf('theme.config') !== -1 || file.path.indexOf('site.variables') !== -1);
-      isPackagedTheme = (file.path.indexOf(source.themes) !== -1);
-      isSiteTheme     = (file.path.indexOf(source.site) !== -1);
-      isDefinition    = (file.path.indexOf(source.definitions) !== -1);
-
-      if(isConfig) {
-        console.info('Rebuilding all UI');
-        // impossible to tell which file was updated in theme.config, rebuild all
-        buildCSS();
-        return;
-      }
-      else if(isPackagedTheme) {
-        console.log('Change detected in packaged theme');
-        lessPath = replaceExt(file.path, '.less');
-        lessPath = lessPath.replace(tasks.regExp.theme, source.definitions);
-      }
-      else if(isSiteTheme) {
-        console.log('Change detected in site theme');
-        lessPath = replaceExt(file.path, '.less');
-        lessPath = lessPath.replace(source.site, source.definitions);
-      }
-      else {
-        console.log('Change detected in definition');
-        lessPath = file.path;
-      }
-
-      /*--------------
-         Create CSS
-      ---------------*/
-
-      if( fs.existsSync(lessPath) ) {
-
-        // unified css stream
-        stream = gulp.src(lessPath)
-          .pipe(plumber(settings.plumber.less))
-          .pipe(less(settings.less))
-          .pipe(print(log.created))
-          .pipe(replace(comments.variables.in, comments.variables.out))
-          .pipe(replace(comments.license.in, comments.license.out))
-          .pipe(replace(comments.large.in, comments.large.out))
-          .pipe(replace(comments.small.in, comments.small.out))
-          .pipe(replace(comments.tiny.in, comments.tiny.out))
-          .pipe(autoprefixer(settings.prefix))
-          .pipe(gulpif(config.hasPermission, chmod(config.permission)))
-        ;
-
-        // use 2 concurrent streams from same pipe
-        uncompressedStream = stream.pipe(clone());
-        compressedStream   = stream.pipe(clone());
-
-        uncompressedStream
-          .pipe(plumber())
-          .pipe(replace(assets.source, assets.uncompressed))
-          .pipe(gulp.dest(output.uncompressed))
-          .pipe(print(log.created))
-          .on('end', function() {
-            (gulp.task(gulp.series('package uncompressed css')))();
-          })
-        ;
-
-        compressedStream
-          .pipe(plumber())
-          .pipe(replace(assets.source, assets.compressed))
-          .pipe(minifyCSS(settings.minify))
-          .pipe(rename(settings.rename.minCSS))
-          .pipe(gulp.dest(output.compressed))
-          .pipe(print(log.created))
-          .on('end', function() {
-            (gulp.task(gulp.series('package compressed css')))();
-          })
-        ;
-      }
-      else {
-        console.log('Cannot find UI definition at path', lessPath);
-      }
-    })
-  ;
+      compressedStream
+        .pipe(plumber())
+        .pipe(replace(assets.source, assets.compressed))
+        .pipe(minifyCSS(settings.minify))
+        .pipe(rename(settings.rename.minCSS))
+        .pipe(gulp.dest(output.compressed))
+        .pipe(print(log.created))
+        .on('end', function() {
+          gulp.task('package compressed css')();
+        })
+      ;
+    }
+    else {
+      console.log('Cannot find UI definition at path', lessPath);
+    }
+  });
 
   /*--------------
       Watch JS
   ---------------*/
 
-  gulp
-    .watch([
-      source.definitions   + '/**/*.js'
-    ], function(file) {
-      gulp.src(file.path)
-        .pipe(plumber())
-        .pipe(replace(comments.license.in, comments.license.out))
-        .pipe(gulpif(config.hasPermission, chmod(config.permission)))
-        .pipe(gulp.dest(output.uncompressed))
-        .pipe(print(log.created))
-        .pipe(uglify(settings.uglify))
-        .pipe(rename(settings.rename.minJS))
-        .pipe(gulp.dest(output.compressed))
-        .pipe(print(log.created))
-        .on('end', function() {
-          (gulp.task(gulp.series('package compressed js', 'package uncompressed js')))();
-        })
-      ;
-    })
-  ;
+  const jsWatcher = gulp.watch([
+    source.definitions   + '/**/*.js'
+  ]);
+
+  jsWatcher.on('change', (path) => {
+    gulp.src(path)
+      .pipe(plumber())
+      .pipe(replace(comments.license.in, comments.license.out))
+      .pipe(gulpif(config.hasPermission, chmod(config.permission)))
+      .pipe(gulp.dest(output.uncompressed))
+      .pipe(print(log.created))
+      .pipe(uglify(settings.uglify))
+      .pipe(rename(settings.rename.minJS))
+      .pipe(gulp.dest(output.compressed))
+      .pipe(print(log.created))
+      .on('end', function() {
+        gulp.task('package compressed js')();
+        gulp.task('package uncompressed js')();
+      });
+  });
 
   /*--------------
     Watch Assets
   ---------------*/
 
   // only copy assets that match component names (or their plural)
-  gulp
-    .watch([
-      source.themes   + '/**/assets/**/*.*'
-    ], function(file) {
-      // copy assets
-      gulp.src(file.path, { base: source.themes })
-        .pipe(gulpif(config.hasPermission, chmod(config.permission)))
-        .pipe(gulp.dest(output.themes))
-        .pipe(print(log.created))
-      ;
-    })
-  ;
+  const assetsWatcher = gulp.watch([
+    source.themes   + '/**/assets/**/*.*'
+  ]);
+
+  assetsWatcher.on('change', (path) => {
+    // copy assets
+    gulp.src(path, { base: source.themes })
+      .pipe(gulpif(config.hasPermission, chmod(config.permission)))
+      .pipe(gulp.dest(output.themes))
+      .pipe(print(log.created));
+  });
 
 };
